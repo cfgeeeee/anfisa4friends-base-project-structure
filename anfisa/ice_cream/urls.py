9d5b057f32f9a304ced6bfb183b1887571b4f587
РљСѓР»Р°РіРёРЅ РџР°РІРµР» <culagin@me.com>
2023-01-31 15:16:36 +0300

    Исправление ошибки в urls.py

--- anfisa/ice_cream/urls.py
+++ anfisa/ice_cream/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('ice_cream/<int:<pk>/', views.ice_cream_detail),
+    path('ice_cream/<int:pk>/', views.ice_cream_detail),
     path('ice_cream/', views.ice_cream_list),
 ]
