8226fdff75fe209e5f66fdb9b796d56173144ce9
РљСѓР»Р°РіРёРЅ РџР°РІРµР» <culagin@me.com>
2023-01-31 13:41:17 +0300

    Связь urls и views. HttpResponse

--- anfisa/ice_cream/views.py
+++ anfisa/ice_cream/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def ice_cream_list(request):
+    return HttpResponse('Список мороженого')
+
+
+def ice_cream_detail(request, pk):
+    return HttpResponse(f'Мороженое номер {pk}')
